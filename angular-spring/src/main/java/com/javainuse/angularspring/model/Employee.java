cbcdf92e2c2c4ef256e32a25fd94be81e150cf0d
gmamenis <gmamenis@iu.edu>
2020-02-21 16:26:26 -0500

    created angular project

--- angular-spring/src/main/java/com/javainuse/angularspring/model/Employee.java
+++ angular-spring/src/main/java/com/javainuse/angularspring/model/Employee.java
@@ -41,46 +41,46 @@ public class Employee {
         this.empId = empId;
     }
 
-    @Override
-    public int hashCode() {
-        final int prime = 31;
-        int result = 1;
-        result = prime * result + ((designation == null) ? 0 : designation.hashCode());
-        result = prime * result + ((empId == null) ? 0 : empId.hashCode());
-        result = prime * result + ((name == null) ? 0 : name.hashCode());
-        long temp;
-        temp = Double.doubleToLongBits(salary);
-        result = prime * result + (int) (temp ^ (temp >>> 32));
-        return result;
-    }
-
-    @Override
-    public boolean equals(Object obj) {
-        if (this == obj)
-            return true;
-        if (obj == null)
-            return false;
-        if (getClass() != obj.getClass())
-            return false;
-        Employee other = (Employee) obj;
-        if (designation == null) {
-            if (other.designation != null)
-                return false;
-        } else if (!designation.equals(other.designation))
-            return false;
-        if (empId == null) {
-            if (other.empId != null)
-                return false;
-        } else if (!empId.equals(other.empId))
-            return false;
-        if (name == null) {
-            if (other.name != null)
-                return false;
-        } else if (!name.equals(other.name))
-            return false;
-        if (Double.doubleToLongBits(salary) != Double.doubleToLongBits(other.salary))
-            return false;
-        return true;
-    }
+//    @Override
+//    public int hashCode() {
+//        final int prime = 31;
+//        int result = 1;
+//        result = prime * result + ((designation == null) ? 0 : designation.hashCode());
+//        result = prime * result + ((empId == null) ? 0 : empId.hashCode());
+//        result = prime * result + ((name == null) ? 0 : name.hashCode());
+//        long temp;
+//        temp = Double.doubleToLongBits(salary);
+//        result = prime * result + (int) (temp ^ (temp >>> 32));
+//        return result;
+//    }
+//
+//    @Override
+//    public boolean equals(Object obj) {
+//        if (this == obj)
+//            return true;
+//        if (obj == null)
+//            return false;
+//        if (getClass() != obj.getClass())
+//            return false;
+//        Employee other = (Employee) obj;
+//        if (designation == null) {
+//            if (other.designation != null)
+//                return false;
+//        } else if (!designation.equals(other.designation))
+//            return false;
+//        if (empId == null) {
+//            if (other.empId != null)
+//                return false;
+//        } else if (!empId.equals(other.empId))
+//            return false;
+//        if (name == null) {
+//            if (other.name != null)
+//                return false;
+//        } else if (!name.equals(other.name))
+//            return false;
+//        if (Double.doubleToLongBits(salary) != Double.doubleToLongBits(other.salary))
+//            return false;
+//        return true;
+//    }
 
 }
