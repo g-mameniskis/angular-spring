e0fc25c2aed7794eabb225d4d84a6270f63f7722
Grayson Mameniskis <57866995+g-mameniskis@users.noreply.github.com>
2020-02-21 13:13:03 -0500

    Add files via upload

---
+++ angular-spring/src/test/java/com/javainuse/angularspring/AngularSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javainuse.angularspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AngularSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
